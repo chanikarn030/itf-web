af56dfa70a0a1c23070e9b1b9c2ef9365177d568
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-06 17:55:11 +0700

    Create cardweb.php

---
+++ cardweb.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Hello</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<style>
+    body{
+        background-color: dodgerblue;
+    }
+</style>
+<body>
+    <div class="container" style="padding: 25px;">
+        <div class="row">
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/picture/starrynight.jpg" style="height: 300px;" >
+                    <div class="card-body">
+                        <h1 class="card-title">Starrynight</h1>
+                        <p class="card-text">is an oil on canvas painting by Dutch Post-Impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence<p>
+                    <a href="#">Artist : Vincent van Gogh</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/picture/scream.jpg" style="height: 300px;">
+                    <div class="card-body">
+                        <h1 class="card-title">The Scream</h1>
+                        <p class="card-text">is the popular name given to a composition created by Norwegian Expressionist artist Edvard Munch in 1893. The original German title given by Munch to his work was Der Schrei der Natur (The Scream of Nature)<p>
+                    <a href="#">Artist : Edvard Munch</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+                    <img class="card-img-top" src="img/picture/Girl.jpg" style="height: 300px;">
+                    <div class="card-body">
+                        <h1 class="card-title">Girl Before A Mirror</h1>
+                        <p class="card-text">was painted in March 1932. It was produced in the style Picasso was using at the time and evoked an image of Vanity such as had been utilized in art in earlier eras<p>
+                    <a href="#">Artist : Pablo Ruiz Picasso,</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
