f0be29ac0040f1339543e0f5220ae18afcaaf3de
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-16 21:18:50 +0700

    Update show.php

--- show.php
+++ show.php
@@ -17,18 +17,18 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook002');
 ?>
 <table class="center" width="600" border="1">
   <tr>
-    <th width="150", style="color: #f0ffff; background-color: #74b164;"> <div align="center">Name</div></th>
-    <th width="300", style="color: #f0ffff; background-color: #88d373;"> <div align="center">Comment </div></th>
-    <th width="150", style="color: #f0ffff; background-color: #74b164;"> <div align="center">Action</div></th>
+    <th width="150"> <div align="center">Name</div></th>
+    <th width="300"> <div align="center">Comment </div></th>
+    <th width="150"> <div align="center">Action</div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td width="300", style="color: #f0ffff; background-color: #88d373;"><?php echo $Result['Name'];?></div></td>
-    <td width="150", style="color: #f0ffff; background-color: #74b164;"><?php echo $Result['Comment'];?></td>
-    <td width="300", style="color: #f0ffff; background-color: #88d373;"><?php echo $Result['Link'];?></td>
+    <td width="300"><?php echo $Result['Name'];?></div></td>
+    <td width="150"><?php echo $Result['Comment'];?></td>
+    <td width="300"><?php echo $Result['Link'];?></td>
   </tr>
 <?php
 }
