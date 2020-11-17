672dc48e17b9fd0fd7561a46475c305c2d4b20bf
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-17 20:40:13 +0700

    Update deletet.php

--- deletet.php
+++ deletet.php
@@ -1,10 +1,18 @@
-<meta charset="UTF-8">
 <?php
-include('connection.php');
-$ID = $_REQUEST["ID"];
 
-$sql = "DELETE FROM guestbook002 WHERE ID='$ID' ";
-$result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
+$link = $_POST['Link'];
+
+$sql = "DELETE FROM guestbook002 WHERE ID='".$_GET["ID"]."'";
+$query = mysqli_query($itflab, $sql);
 
 echo "<a href=\"show.php\">BACK</a>";
 ?>
