88218637058fc28c965d8adef615c77ec0d35202
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-17 20:31:36 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,6 @@
+<?php
+$con= mysqli_connect("dataitf.mysql.database.azure.com","laize@dataitf","08032545Pu","itflab", 3306) or die("Error: " . mysqli_error($con));
+ 
+mysqli_query($con, "SET NAMES 'utf8' ");
+ 
+?>
