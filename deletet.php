78c6e34eecc1b234b9fe004f879687fcd6d111ba
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-17 20:08:13 +0700

    Update deletet.php

--- deletet.php
+++ deletet.php
@@ -1,15 +1,12 @@
 <?php
 
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
-if ( $_GET['ID']) {
-    $ID = $_GET["ID"];
-} else {
-    echo "ID not found.";
-    die();
+$conn = mysqli_connect('dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
+if ($conn === false) {
+    die("ERROR: could not connect.".mysqli_connect_error());
+} else{
+    $ID = $_GET['ID']
 }
-
-$sql = "DELETE FROM 'guestbook002' WHERE ID = {$ID}";
+$sql = "DELETE FROM guestbook002 WHERE ID = {$ID}";
 $query = mysqli_query($itflab, $sql);
 
 echo "<a href=\"show.php\">BACK</a>";
