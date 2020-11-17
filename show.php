38a8c7d7528a20e091b1509ed072ebbaac36d6e0
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-17 17:22:07 +0700

    Update show.php

--- show.php
+++ show.php
@@ -35,7 +35,7 @@ while($Result = mysqli_fetch_array($res))
     <td width="300" style="background-color: #f8c6ce"><?php echo $Result['Name'];?></div></td>
     <td width="150" style="background-color: #fabcb6"><?php echo $Result['Comment'];?></td>
     <td width="300" style="background-color: #f8c6ce"><div align="center">
-        <input type="button" value="Delete" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white" onclick="document.location='show.php'">
+        <input type="button" value="Delete" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white" onclick="document.location='deletet.php'">
         <button type="button" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color: white;" value="Edit"></div></td>
   </tr>
 <?php
