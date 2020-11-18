3ccd8a4f8a96465dbf1168b89119fdb8db430f96
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-18 16:16:34 +0700

    Update show.php

--- show.php
+++ show.php
@@ -37,7 +37,7 @@ while($Result = mysqli_fetch_array($res))
     <td width="150" style="background-color: #f8c6ce"><?php echo $Result['Comment'];?></td>
     <td width="150" style="background-color: #fabcb6"><?php echo $Result['Link'];?></td>
     <td width="300" style="background-color: #f8c6ce"><div align="center">
-        <input type="button" value="Delete" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white"><a href="deletet.php?id=$_GET['ID']"></a>
+        <a href="deletet.php?id=$_GET['ID']" class="button" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white">Delete</a>
         <input type="button" value="Edit" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white">
   </tr>
 <?php
@@ -51,4 +51,3 @@ mysqli_close($conn);
 ?>
 </body>
 </html>
-
