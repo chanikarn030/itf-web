c4cfba478f1f5a3673406c5a84d8cfc2ccf9193b
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-18 22:04:19 +0700

    Update show.php

--- show.php
+++ show.php
@@ -58,6 +58,37 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook002');
 ?>
 <table class="center" width="750" border="1">
   <tr>
+<html>
+<head>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<style>
+    th, td {padding: 6px; border-inline-end: 0cm; border-bottom: 0cm; border-top: 0cm; border-inline-start: 0cm;}
+    .center1{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 585px;
+    height: 100px}
+</style>
+<body>
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook002');
+?>
+<table class="center" width="750" border="1">
+  <tr>
     <th width="150" style="color: #f0ffff; background-color: salmon;"><div align="center">Name</div></th>
     <th width="300" style="color: #f0ffff; background-color: lightcoral;"><div align="center">Comment</div></th>
     <th width="150" style="color: #f0ffff; background-color: salmon;"><div align="center">Link</div></th>
@@ -72,7 +103,7 @@ while($Result = mysqli_fetch_array($res))
     <td width="150" style="background-color: #f8c6ce"><?php echo $Result['Comment'];?></td>
     <td width="150" style="background-color: #fabcb6"><?php echo $Result['Link'];?></td>
     <td width="300" style="background-color: #f8c6ce"><div align="center">
-        <button type="submit" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white"><a href="deletet.php?id=$row['id']">">Delete</a></button>
+        <button type="submit" style="padding: 3.5%; background-color: tomato; border-width: 0cm; color:white"><a href="deletet.php?id=$Result['id']">">Delete</a></button></td>
   </tr>
 <?php
 }
@@ -85,4 +116,3 @@ mysqli_close($conn);
 ?>
 </body>
 </html>
-
