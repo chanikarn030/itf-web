34336e660da7cf4a7a9a558d45de320463342b8e
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-19 13:47:00 +0700

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+
+$name = $_POST['Name'];
+$comment = $_POST['Comment'];
+$link = $_POST['Link'];
+$id = $_POST['id'];
+
+$sql = "UPDATE guestbook002 SET Name=$name, Comment=$comment, Link=$link WHERE id=$id";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
