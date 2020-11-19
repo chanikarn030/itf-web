64c602e707db8d016b34d2e33b2b6fdfb4084fe7
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-19 13:48:44 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -11,7 +11,7 @@ $comment = $_POST['Comment'];
 $link = $_POST['Link'];
 $id = $_POST['id'];
 
-$sql = "UPDATE guestbook002 SET Name=$name, Comment=$comment, Link=$link WHERE id=$id";
+$sql = "UPDATE guestbook002 SET Name=$name, Comment=$comment, Link=$link WHERE id=9";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
