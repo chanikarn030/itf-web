0efbf5f42defff9e5c06e7ffafb1168e419cf70e
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-27 10:02:17 +0700

    Update show2.php

--- show2.php
+++ show2.php
@@ -34,12 +34,12 @@
 <body>
 <?php
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'it63070030_itf', 'it63070030_chanikarn', '08032545Pu', 'it63070030_itf', 3306);
+mysqli_real_connect($conn, 'dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$res = mysqli_query($conn, 'SELECT * FROM guestbook003');
 ?>
 <table class="center1" width="1100" style="margin-left:auto;margin-right:auto;margin-top: 20px;">
   <tr>
