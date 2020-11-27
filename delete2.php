c4d1c9377b59c49c95d48d171a6e49a2020482a8
chanikarn030 <73680793+chanikarn030@users.noreply.github.com>
2020-11-27 10:14:15 +0700

    Create delete2.php

---
+++ delete2.php
@@ -0,0 +1,22 @@
+  
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'dataitf.mysql.database.azure.com', 'laize@dataitf', '08032545Pu', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+$id = $_GET['Id'];
+
+$sql = "DELETE FROM guestbook003 WHERE Id = $id";
+
+
+if (mysqli_query($conn, $sql)) {
+    header('location: show2.php');
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
